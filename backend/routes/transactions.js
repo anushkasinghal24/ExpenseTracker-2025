166ef80a91ac815045d3beaa0d7c25d08017fe74
anushkasinghal24 <singhalanushka23@gmail.com>
2025-03-12 18:21:01 +0530

    Backend of the project

---
+++ backend/routes/transactions.js
@@ -0,0 +1,16 @@
+const { getExpense, deleteExpense, addExpense } = require('../controllers/expense');
+const { addIncome , getIncomes, deleteIncomes} = require('../controllers/income')
+
+const router = require('express').Router();
+
+
+router.post('/add-income', addIncome)
+   .get('/get-incomes', getIncomes)
+   .delete('/delete-income/:id', deleteIncomes)
+   .post('/add-expenses', addExpense)
+   .get('/get-expenses', getExpense)
+   .delete('/delete-expenses/:id', deleteExpense)
+
+
+
+module.exports = router; 
